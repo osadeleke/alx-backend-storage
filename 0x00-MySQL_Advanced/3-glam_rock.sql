d61cb6d3d921cd1fd8f4c02519dbcd000063128f
Olusegun Adeleke <oadeleke123@gmail.com>
2024-04-25 16:12:06 +0100

    fix year from 2020 to 2022

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,7 +1,7 @@
 -- check for style
 
 SELECT band_name,
-CASE WHEN split IS NULL THEN 2020 - formed ELSE split - formed END AS lifespan
+CASE WHEN split IS NULL THEN 2022 - formed ELSE split - formed END AS lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam Rock%'
 ORDER BY lifespan DESC;
