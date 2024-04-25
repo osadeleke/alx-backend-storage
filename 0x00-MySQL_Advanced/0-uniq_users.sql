1ecacd800890b0c99b6a2f0117c1390aa22bee48
Olusegun Adeleke <oadeleke123@gmail.com>
2024-04-25 14:28:12 +0100

    correction users not user

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,7 @@
 -- SQL script that creates a table users
 
 -- create table
-CREATE TABLE IF NOT EXISTS user (
+CREATE TABLE IF NOT EXISTS users (
         id INT AUTO_INCREMENT PRIMARY KEY,
         email VARCHAR(255) NOT NULL UNIQUE,
         name VARCHAR(255)
