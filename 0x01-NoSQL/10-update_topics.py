c57c864a738df195fe7d718b2b83140c5f337d1c
Olusegun Adeleke <oadeleke123@gmail.com>
2024-04-30 19:01:39 +0100

    Update topic in document

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -6,6 +6,9 @@ from pymongo import MongoClient
 
 
 def update_topics(mongo_collection, name, topics):
+    """
+    update a topic in document
+    """
     mongo_collection.update_many(
       {'name': name},
       {'$set': {'topics': topics}}
