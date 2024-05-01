1740077a329560176f3af04d09f6225bb6d7894c
Olusegun Adeleke <oadeleke123@gmail.com>
2024-05-01 16:44:17 +0100

    Update store method in Cache class to accept a wider range of data types in exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,19 +4,19 @@ write to redis
 """
 import redis
 from uuid import uuid4
-from typing import Any
+from typing import Union
 
 
 class Cache():
     """
     write to redis
     """
-    def __init__(self):
+    def __init__(self) -> None:
         """initialize redis in class"""
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: Any) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """get uuid key and save data"""
         keystore = str(uuid4())
         self._redis.set(keystore, data)
